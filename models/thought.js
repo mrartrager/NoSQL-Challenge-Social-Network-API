ad232c2ba779303fed802522d3dd7068a5ab4ddf
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-01-14 10:55:00 -0600

    thought Model should be completed

---
+++ models/thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction')
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new Date(timestamp).toDateString(),
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+thoughtSchema.virtual('reactionSchema').get(function (){
+    return this.reactions.length;
+});
+
+const thought = model('User', userSchema);
+
+module.export = thought;
